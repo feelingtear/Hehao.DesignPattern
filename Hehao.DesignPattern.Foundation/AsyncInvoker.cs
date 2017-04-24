1a3638a78daa9136f3358cb080297704e7b191f6
Allen <dh858115@126.com>
2017-04-24 20:43:42 +0800

    20170424

---
+++ Hehao.DesignPattern.Foundation/AsyncInvoker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Hehao.DesignPattern.Foundation
+{
+    public class AsyncInvoker
+    {
+        private IList<string> output = new List<string>();
+
+        public AsyncInvoker()
+        {
+            Timer slowTimer = new Timer(new TimerCallback(OnTimerInterval), "slow", 2500, 2500);
+            Timer fastTimer = new Timer(new TimerCallback(OnTimerInterval), "fast", 2000, 2000);
+            output.Add("method");
+        }
+
+        private void OnTimerInterval(object state)
+        {
+            output.Add(state as string);
+        }
+
+        public IList<string> Ouput
+        {
+            get
+            {
+                return output;
+            }
+        }
+    }
+}
---
+++ Hehao.DesignPattern.UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hehao.DesignPattern.Foundation;
+
+namespace Hehao.DesignPattern.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AsyncInvoker asyncInvoke = new AsyncInvoker();
+            System.Threading.Thread.Sleep(3000);
+
+            Assert.AreEqual<string>("method", asyncInvoke.Ouput[0]);
+            Assert.AreEqual<string>("fast", asyncInvoke.Ouput[1]);
+            Assert.AreEqual<string>("slow", asyncInvoke.Ouput[2]);
+        }
+    }
+}
