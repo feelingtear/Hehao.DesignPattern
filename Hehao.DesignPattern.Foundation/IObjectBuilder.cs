e9a8224dfc6f0dcef7ad4635909e6ac2c510c085
Allen <dh858115@126.com>
2017-06-12 20:42:50 +0800

    20170612

---
+++ Hehao.DesignPattern.Foundation/IObjectBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hehao.DesignPattern.Foundation
+{
+    public interface IObjectBuilder
+    {
+        T BuildUp<T>(object[] args);
+
+        T BuildUp<T>() where T:new();
+
+        T BuildUp<T>(string typename);
+
+        T BuildUp<T>(string typename, object[] args);
+    }
+}
